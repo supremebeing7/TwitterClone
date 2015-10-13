6dfb2456e11150ae6c0bc6e8a9de2e68b4b0a671
Mark Lehman <markopolo@gmail.com>
2015-10-13 14:05:11 -0700

    Fill out models

--- app/src/main/java/com/markjlehman/twitterclone/models/Tweet.java
+++ app/src/main/java/com/markjlehman/twitterclone/models/Tweet.java
@@ -1,4 +1,62 @@
 package com.markjlehman.twitterclone.models;
 
-public class Tweet {
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Table(name = "Tweets", id = "_id")
+public class Tweet extends Model {
+    @Column(name = "User")
+    private User mUser;
+
+    @Column(name = "CreatedAt")
+    private long mCreatedAt;
+
+    @Column(name = "Content")
+    private String mContent;
+
+    public Tweet(User mUser, String mContent) {
+        super();
+        this.mUser = mUser;
+        this.mContent = mContent;
+        this.mCreatedAt = new Date().getTime();
+    }
+
+    public Tweet() {
+        super();
+    }
+
+    public User getmUser() {
+        return mUser;
+    }
+
+    public void setmUser(User mUser) {
+        this.mUser = mUser;
+    }
+
+    public long getmCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public void setmCreatedAt(long mCreatedAt) {
+        this.mCreatedAt = mCreatedAt;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMMM d 'at' h:mm");
+        formatter.setTimeZone(TimeZone.getTimeZone("PST"));
+        return formatter.format(mCreatedAt);
+    }
 }
--- app/src/main/java/com/markjlehman/twitterclone/models/User.java
+++ app/src/main/java/com/markjlehman/twitterclone/models/User.java
@@ -1,4 +1,40 @@
 package com.markjlehman.twitterclone.models;
 
-public class User {
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "Users", id = "_id")
+public class User extends Model {
+    @Column(name = "Name")
+    private String mName;
+
+    @Column(name = "Email")
+    private String mEmail;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmEmail() {
+        return mEmail;
+    }
+
+    public void setmEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public User(String mName, String mEmail) {
+        super();
+        this.mName = mName;
+        this.mEmail = mEmail;
+    }
+
+    public User() {
+        super();
+    }
 }
